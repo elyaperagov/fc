6c96f32d24b03d452ef29ec3de96b93b3b027901
elyaperagov <piragof@gmail.com>
2022-02-14 15:39:59 +0300

    добавляет правки видео

--- src/components/home/Services.vue
+++ src/components/home/Services.vue
@@ -4,10 +4,10 @@
       <h2>Services</h2>
       <div class="services__list">
         <div v-for="(item, i) in items" :key="i" class="services__item">
-          <video class="services__video" :poster="item.poster" muted>
+          <video class="services__video" :poster="item.poster" playsinline muted>
             <source :src="item.video" type="video/mp4" />
-            <!-- <source src="@/assets/media/main_alt.ogv" type="video/ogg" /> -->
-            <!-- <source src="@/assets/media/main_alt.webm" type="video/webm" /> -->
+            <source :src="item.videoOgg" type="video/ogg" />
+            <source :src="item.videoWebm" type="video/webm" />
           </video>
           <p class="services__text" v-html="item.text"></p>
         </div>
@@ -25,16 +25,22 @@ export default {
       items: [
         {
           video: require('@/assets/media/Smart_contracts_video.mp4'),
+          videoWebm: require('@/assets/media/Smart_contracts_video.webm'),
+          videoOgg: require('@/assets/media/Smart_contracts_video.mp4'),
           poster: require('@/assets/img/Smart.jpg'),
           text: 'Smart contracts'
         },
         {
           video: require('@/assets/media/Blockchain.mp4'),
+          videoWebm: require('@/assets/media/Blockchain.webm'),
+          videoOgg: require('@/assets/media/Blockchain.mp4'),
           poster: require('@/assets/img/Blockchain.jpg'),
           text: 'Blockchain'
         },
         {
           video: require('@/assets/media/Audit.mp4'),
+          videoWebm: require('@/assets/media/Audit.webm'),
+          videoOgg: require('@/assets/media/Audit.mp4'),
           poster: require('@/assets/img/Audit.jpg'),
           text: 'Audit and verification'
         }
